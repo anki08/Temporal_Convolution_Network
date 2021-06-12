5eee5b708256ebbe480640bd925e3dd86993b6a0
asinha4 <ankitasinha0811@gmail.com>
2021-06-12 12:51:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,32 +2,24 @@ from ignite.contrib.handlers import FastaiLRFinder, ProgressBar
 from ignite.engine import create_supervised_trainer, create_supervised_evaluator
 from ignite.metrics import Loss
 
-from .models import TCN, save_model
-from .utils import one_hot, load_speech_data
+import utils
+from models import TCN, save_model
 
 
 def train(args):
-    from os import path
-    import torch.utils.tensorboard as tb
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train_7'), flush_secs=1)
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid_7'), flush_secs=1)
 
     import torch
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     model = TCN().to(device)
-    if args.continue_training:
-        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'tcn.th')))
     loss = torch.nn.NLLLoss()
 
     optimizer = torch.optim.SGD(model.parameters(), lr=0.5, momentum=0.9)
 
     batch = 1
-    train_data = load_speech_data('<path to train data>', transform=one_hot, batch_size=batch)
-    valid_data = load_speech_data('<path to test data>', transform=one_hot, batch_size=batch)
+    train_data = load_speech_data('<path to train data>', transform=utils.one_hot, batch_size=batch)
+    valid_data = load_speech_data('<path to test data>', transform=utils.one_hot, batch_size=batch)
 
     trainer = create_supervised_trainer(model, optimizer, loss, device=device)
     ProgressBar(persist=True).attach(trainer, output_transform=lambda x: {"batch loss": x})
@@ -53,7 +45,6 @@ def train(args):
             batch_label = batches.argmax(dim=1).to(device)
             o = model(batch_data)
             loss_val = loss(o, batch_label)
-            train_logger.add_scalar('train/loss', loss_val, global_step=global_step)
             global_step += 1
             optimizer.zero_grad()
             loss_val.backward()
@@ -66,20 +57,9 @@ def train(args):
             batch_label_valid = data.argmax(dim=1).to(device)
             o = model(batch_data_valid)
             loss_valid = loss(o, batch_label_valid)
-            valid_logger.add_scalar('valid/loss', loss_valid, global_step=global_step)
 
-        print("iterations: ", iterations)
+        print("validation loss: ", loss_valid)
 
 
 if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--log_dir')
-    parser.add_argument('-n', '--n_iterations', type=int, default=20)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=0.1)
-    parser.add_argument('-c', '--continue_training', action='store_true')
-
-    args = parser.parse_args()
-    train(args)
+    train()
